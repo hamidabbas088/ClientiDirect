5cfd91d9606d667c8076f8b9335e31485ce72454
hamidabbas088 <hamidurani2002@gmail.com>
2024-10-31 01:43:53 +0500

    fix: resolve issue with pricing calculation

--- src/utils/someutility.ts
+++
@@ -1,7 +0,0 @@
-// utils/someUtility.js
-export function getBrowserWidth() {
-  if (typeof window !== 'undefined') {
-    return window.innerWidth;
-  }
-  return null; // Return a fallback value if on the server
-}
